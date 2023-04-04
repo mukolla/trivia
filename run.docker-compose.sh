a4a592f1d0d3089085b45ca536d4b767b3cc73ed
mukolla <mukolla@gmail.com>
2023-04-04 12:31:26 +0300

    upd

--- run.docker-compose.sh
+++ run.docker-compose.sh
@@ -14,7 +14,7 @@ log_message "Stopping containers..."
 docker compose -p trivia_api stop
 
 log_message "Removing containers..."
-docker compose -p trivia_api_app rm -f
+docker compose -p trivia_api rm -f
 
 log_message "Starting containers..."
-docker compose -p trivia_api_app up -d --remove-orphans --build
+docker compose -p trivia_api up -d --remove-orphans --build
